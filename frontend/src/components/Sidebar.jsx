09c574fa97e8ced7a2ad9222122b48b8334549c5
lucasevangelista <levangelista0800@gmail.com>
2026-07-04 14:02:30 -0300

    feat: Adicionando informações no Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      {/* O usuário pediu para deixar completamente em branco */}
+      <div className="sidebar-header">
+        <h2 className="sidebar-logo">MineraSys</h2>
+        <p className="sidebar-subtitle">Plataforma para gestão de operações e logística na mineração.</p>
+        <br />
+        <p style={{ fontSize: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <FaInstagram size={30} color="#E4405F" /> @MineraSys
+        </p>
+        <br />
+        <p style={{ fontSize: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <FaWhatsapp size={30} color="#25D366" />(88) 4002-8922
+        </p>
+
+      </div>
     </aside>
   );
 };
