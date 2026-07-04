109dd890c1a6d69db0e2ca339c53d4ab01242287
lucasevangelista <levangelista0800@gmail.com>
2026-07-04 15:07:32 -0300

    Configuracao para producao

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.DEV
     ? '/api'
-    : import.meta.env.VITE_API_URL
+    : `${import.meta.env.VITE_API_URL}/router`
 });
 
 export default api;
